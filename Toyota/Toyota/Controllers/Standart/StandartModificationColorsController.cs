631fbb82748f0961c0ccfeec6439b85eca8edaff
IsypMykhailo <amazonkanameforme@gmail.com>
2022-01-26 14:18:34 +0200

    modification colors

--- Toyota/Toyota/Controllers/Standart/StandartModificationColorsController.cs
+++ Toyota/Toyota/Controllers/Standart/StandartModificationColorsController.cs
@@ -60,7 +60,7 @@ namespace Toyota.Controllers.Standart
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Slug,ModificationId,ColorId,ImgUrl")] ModificationColors modificationColors, IFormFile fileToStorage)
+        public async Task<IActionResult> Create([Bind("Id,Slug,ModificationId,ColorId")] ModificationColors modificationColors, IFormFile fileToStorage)
         {
             if (ModelState.IsValid)
             {
