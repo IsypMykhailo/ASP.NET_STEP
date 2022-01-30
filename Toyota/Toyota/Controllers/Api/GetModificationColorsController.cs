63b88013e1411d745ea07c887212f351cd115c25
IsypMykhailo <amazonkanameforme@gmail.com>
2022-01-30 17:50:34 +0200

    toyota-finish

---
+++ Toyota/Toyota/Controllers/Api/GetModificationColorsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Toyota.Data;
+using Toyota.Data.Entities;
+
+namespace Toyota.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetModificationColorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetModificationColorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Color>>> Get(Guid id)
+        {
+            /*return await _context.ModificationColors
+                .Where(m => m.ModificationId == id)
+                .Include(m=>m.Color)
+                .Where(m=>m.Color.Id == m.ColorId)
+                .ToListAsync();*/
+            /*return await _context.ModificationColors
+                .Include(m => m.Color)
+                .Include(m => m.Modification)
+                .FirstOrDefaultAsync(m => m.Id == id);*/
+            return await _context.Colors
+                    .SelectMany(
+                        color => color.ModificationColors,
+                        (c, mc) => new { Color = c, ModColor = mc })
+                    .Where(a => a.ModColor.ModificationId == id)
+                    .Select(a => a.Color).ToListAsync();
+            /*return await _context.ModificationColors
+                    .Where(a => a.ModificationId == id).ToListAsync();*/
+        }
+    }
+}
